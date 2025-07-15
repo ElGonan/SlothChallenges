4eb0f8c5199e97ef2c6df3905fed77ca9eb531ff
ElGonan <alanpagzz@outlook.com>
2025-07-15 16:41:42 -0600

    weekly done

---
+++ sherlock.py
@@ -0,0 +1,26 @@
+def isValid(input):
+    dic = {}
+
+    for i in input:
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i] +=1
+
+    
+    if max(dic.values()) - min(dic.values()) > 0:
+            maximum = max(dic, key=dic.get)
+            dic[maximum] -= 1
+
+    if max(dic.values()) - min(dic.values()) > 0:
+        return "NO"
+
+    return "YES"
+
+
+print(isValid("abc"))
+print(isValid("abcc"))
+print(isValid("abccc"))
+print(isValid("aabbcd"))
+print(isValid("aabbccddeefghi"))
+print(isValid("abcdefghhgfedecba"))
